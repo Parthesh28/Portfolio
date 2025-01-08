94ae70c459a9efe1fb966a48833fe5e22e606490
Parthesh28 <partheshpurohit28@gmail.com>
2025-01-08 22:50:09 +0530

    fixed theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body className={`${inter.className} flex flex-col min-h-screen`}>
 
         
-          <ThemeProvider defaultTheme='system' attribute="class">
+          <ThemeProvider defaultTheme='system' attribute="class" enableSystem>
             <ActiveSectionContextProvider>
               <Navbar />
               <div className='-z-50'>
